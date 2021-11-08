439168f5c81551a3d1f5f0a0e3ae06f0ea943699
Gronik64 <golegni@yandex.ru>
2021-11-08 22:20:55 +0300

    app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,11 @@
-export default function versionAttack({ special = [] }, nameA) {
+export default function versionAttack(character, nameA) {
+  let {special = []} = character;
   const res = special.find((item) => item.name === nameA);
   if (!('description' in res)) { res.description = 'Описание пока еще недоступно'; }
   const result = [];
   for (const key in res) {
     result.push({ [key]: res[key] });
   }
-  return result;
+  const result1 = [special, result];
+  return result1;
 }
